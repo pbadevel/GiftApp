1561fbfd42718843848d196464db4ba2c3fbcdd6
QWERTYZPB <pavelburykin13@gmail.com>
2025-05-12 02:04:18 +0300

    .

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <meta 
             name="viewport" 
-            content="width=device-width, initial-scale=0.8, maximum-scale=1.0, user-scalable=yes, viewport-fit=cover"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes, viewport-fit=cover"
           />
         <script src="https://telegram.org/js/telegram-web-app.js" async />
       </Head>
