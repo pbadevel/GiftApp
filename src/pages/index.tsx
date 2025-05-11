2e3ddc25f0f1a431a99645d68492b426d0aabc89
QWERTYZPB <pavelburykin13@gmail.com>
2025-05-12 01:37:12 +0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ import styles from '../styles/main-page.module.css';
 export default function GiveawayInterface() {
   const router = useRouter();
   const { eventId } = router.query; // Получаем параметр из URL
-  const [userID, setUserId] = useState('');
-  const [eventID, setEventId] = useState('');
+  const [userID, setUserId] = useState<string>('1');
+  const [eventID, setEventId] = useState<string>('1');
 
 
   const [channels, setChannels] = useState<Channel[]>([]);
