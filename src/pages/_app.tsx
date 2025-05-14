5fbe685c57fa6cbfdea80fb7c82a3a33d3b7985f
QWERTYZPB <pavelburykin13@gmail.com>
2025-05-14 23:08:17 +0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app'
-
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
