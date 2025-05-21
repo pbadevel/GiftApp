4906d9af8b0c1d7f75ec64fa62de98653c9ae0ed
QWERTYZPB <pavelburykin13@gmail.com>
2025-05-22 01:34:48 +0300

    .

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -34,14 +34,16 @@ const RaffleTimer = () => {
         days: 0, 
         hours: 0, 
         minutes: 0, 
-        seconds: 0 
+        seconds: 0 ,
+        users_to_invite:0
       };
   
       return {
         days: Math.floor(total / 86400),
         hours: Math.floor((total % 86400) / 3600),
         minutes: Math.floor((total % 3600) / 60),
-        seconds: total % 60
+        seconds: total % 60,
+        users_to_invite:0
       };
     };
     
