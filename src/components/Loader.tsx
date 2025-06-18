364b3147775d14358fe5efa85a37342e97d3b89f
QWERTYZPB <pavelburykin13@gmail.com>
2025-06-19 01:40:27 +0300

    .

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,26 +4,28 @@ import Image from 'next/image';
 
 
 
-// const LoaderSVG = () => {
-//   return (    
-//     <Image 
-//         src="/loader.webp"
-//         alt=''
-//         width="350"
-//         height="250"
-        
-//     />        
-    
-//   );
-// }
-
 const LoaderSVG = () => {
-  return (
-    <video width="320" height="240" controls preload="none">
-      <source src="/loader.mp4" type="video/mp4" />
-    </video>
-  )
+  return (  
+    <>
+      <Image 
+          src="/loader.webp"
+          alt=''
+          width="350"
+          height="250"
+          
+      />
+      <p style={{ fontSize: "15px" }}>Идет проверка...</p>  
+    </>  
+  );
 }
+
+// const LoaderSVG = () => {
+//   return (
+//     <video width="320" height="240" controls preload="none">
+//       <source src="/loader.mp4" type="video/mp4" />
+//     </video>
+//   )
+// }
 
 
 export default LoaderSVG;
