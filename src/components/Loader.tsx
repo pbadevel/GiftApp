f296eeb010a4358ef6304fd3d5d2ec4302e22bf3
QWERTYZPB <pavelburykin13@gmail.com>
2025-06-19 01:26:13 +0300

    .

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -9,13 +9,14 @@ const LoaderSVG = () => {
     <Image 
         src="/loader.webp"
         alt=''
-        width="200"
-        height="200"
+        width="350"
+        height="250"
         
     />        
     
   );
 }
+
 
 export default LoaderSVG;
 
