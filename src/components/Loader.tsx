2bb546f0306e6397440098d0e155af6d194e63f8
QWERTYZPB <pavelburykin13@gmail.com>
2025-06-19 01:33:37 +0300

    .

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,17 +4,25 @@ import Image from 'next/image';
 
 
 
-const LoaderSVG = () => {
-  return (    
-    <Image 
-        src="/loader.webp"
-        alt=''
-        width="350"
-        height="250"
+// const LoaderSVG = () => {
+//   return (    
+//     <Image 
+//         src="/loader.webp"
+//         alt=''
+//         width="350"
+//         height="250"
         
-    />        
+//     />        
     
-  );
+//   );
+// }
+
+const LoaderSVG = () => {
+  return (
+    <video width="320" height="240" controls preload="none">
+      <source src="/loader.mp4" type="video/mp4" />
+    </video>
+  )
 }
 
 
